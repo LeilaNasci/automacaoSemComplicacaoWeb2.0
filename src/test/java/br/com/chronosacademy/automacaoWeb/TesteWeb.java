0d025b90534a7f90dfb50f8a226b34856dfdcb7f
LeilaNasci <leila.martins@eduzz.com>
2021-07-01 21:05:09 -0300

    Aula 01/07

--- src/test/java/br/com/chronosacademy/automacaoWeb/TesteWeb.java
+++ src/test/java/br/com/chronosacademy/automacaoWeb/TesteWeb.java
@@ -2,6 +2,8 @@ package br.com.chronosacademy.automacaoWeb;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,20 +12,52 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+
 public class TesteWeb {
-	@Test
-	public void primeiroTeste() {
-		WebDriverManager.chromedriver();
-		WebDriver driver = new ChromeDriver();
+	
+	WebDriver driver;
+	
+	@Before
+	public void inicializaTeste() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();		
 		driver.manage().window().maximize();
 		driver.get("https://www.chronosacademy.com.br/");
+		
+	}
+	
+	
+	@Test
+	public void primeiroTeste() {
+		
 		String txtTitulo = "/html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[1]/div/h4";
 		WebElement h4Titulo = driver.findElement(By.xpath(txtTitulo));
 		String titulo = h4Titulo.getText();
 		assertEquals("Porque Tempo É Conhecimento", titulo);
-		driver.quit();
+		
+		
+	}  
 	
-	}                                           
+	@Test
+	public void segundoTeste() {
+		
+		String xpathBotao = "//*[text()=\"Conheça Nossos Cursos\"]";
+		WebElement btnTitulo = driver.findElement(By.xpath(xpathBotao));
+		btnTitulo.click();
+		String txtTitulo = "//*[text()=\"AUTOMAÇÃO SEM COMPLICAÇÃO WEB 2.0\"]";
+		WebElement h2Titulo = driver.findElement(By.xpath(txtTitulo));
+		String titulo = h2Titulo.getText();
+		assertEquals("AUTOMAÇÃO SEM COMPLICAÇÃO WEB 2.0", titulo);
+	
+		
+		
+	}  
+	
+	@After
+	public void fechar() {
+		driver.quit();	
+		
+	}
 	
 
 }
